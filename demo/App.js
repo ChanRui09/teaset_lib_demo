895e03ee017632f3ef500d7178231aa547eb3739
Chanry <chenrui191@huawei-partners.com>
2025-10-14 17:36:57 +0800

    test: 添加集成demo元数据

--- demo/App.js
+++ demo/App.js
@@ -1,11 +1,27 @@
-
 import React, {Component} from 'react';
 
 import {TeaNavigator, Theme} from 'teaset';
 import TeasetExampleHome from './views/Home';
 
-export default class App extends Component<{}> {
+class App extends Component {
   render() {
     return <TeaNavigator rootView={<TeasetExampleHome />} />;
   }
 }
+
+export default {
+  displayName: 'teaset',
+  framework: 'React',
+  category: 'UI',
+  title: 'teaset',
+  documentationURL: 'teaset',
+  description: '',
+  examples: [
+    {
+      title: 'teaset',
+      render: function () {
+        return <App />;
+      },
+    },
+  ],
+};
